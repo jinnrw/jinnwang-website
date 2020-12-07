da8ed1316eaafca42e915ada6444081d1316f0a9
jinnrw <Jinn@JinndeMBP.hitronhub.home>
2020-12-06 16:28:47 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "181080495",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+  ],
+};
